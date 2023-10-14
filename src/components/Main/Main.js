3b831e68d418bb5d44912405e1e769ab59b05b6d
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-14 19:37:46 +0300

    Started product display component

---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import { ProductDisplay } from "./ProductDisplay";
+import {Container} from "../Common"
+export function Main() {
+    return (
+        <Container>
+            <ProductDisplay />
+        </Container>
+    )
+}
---
+++ src/components/Main/ProductDisplay/ProductDisplay.js
@@ -0,0 +1,29 @@
+import ProductOne from "../../../images/image-product-1.jpg"
+import ProductTwo from "../../../images/image-product-2.jpg"
+import ProductThree from "../../../images/image-product-3.jpg"
+import ProductFour from "../../../images/image-product-4.jpg"
+import ProductOneThumbnail from "../../../images/image-product-1-thumbnail.jpg"
+import ProductTwoThumbnail from "../../../images/image-product-2-thumbnail.jpg"
+import ProductThreeThumbnail from "../../../images/image-product-3-thumbnail.jpg"
+import ProductFourThumbnail from "../../../images/image-product-4-thumbnail.jpg"
+import { DisplayButton } from "../../Common"
+import "./ProductDisplay.css"
+const thumbnails = [ProductOneThumbnail, ProductTwoThumbnail, ProductThreeThumbnail, ProductFourThumbnail]
+
+export function ProductDisplay() {
+    return (
+        <section>
+            <div className="row">
+                {/* <img src={ProductOne} alt="product cover" /> */}
+                <DisplayButton source={ProductOne} description="product cover" /* ClickEvent={} *//>
+            </div>
+            <div className="row">
+                    {thumbnails.map((image) => (
+                        <li>
+                            <DisplayButton source={image} description="product thumbnail" /* ClickEvent={} *//>
+                        </li>
+                    ))}
+            </div>
+        </section>
+    )
+}
