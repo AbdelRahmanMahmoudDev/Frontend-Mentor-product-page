e021a5f4bee871a6f9b88768957b4c650242a170
AbdelRahmanMahmoudDev <abdelrahmanmahmoud801@gmail.com>
2023-10-16 17:41:52 +0300

    [FEATURE]: Added feature where clicking on cover page displays it on the overlay

--- src/components/Common/DisplayButton/DisplayButton.js
+++ src/components/Common/DisplayButton/DisplayButton.js
@@ -2,7 +2,7 @@ import "./DisplayButton.css"
 
 export function DisplayButton(props) {
     return (
-        <button  className="btn-display" onClick={props.onClick}>
+        <button style={{...props.style}} className="btn-display" onClick={props.onClick}>
             <img className={props.className} src={props.source} alt={props.description}/>
         </button>
     )
--- src/components/Main/ProductDisplay/ProductDisplay.js
+++ src/components/Main/ProductDisplay/ProductDisplay.js
@@ -9,7 +9,8 @@ import ProductThreeThumbnail from "../../../images/image-product-3-thumbnail.jpg
 import ProductFourThumbnail from "../../../images/image-product-4-thumbnail.jpg"
 import IconNext from "../../../images/icon-next.svg"
 import IconPrevious from "../../../images/icon-previous.svg"
-import { DisplayButton, Container } from "../../Common"
+import IconClose from "../../../images/icon-close.svg"
+import { DisplayButton, Container, Overlay } from "../../Common"
 import "./ProductDisplay.css"
 import {useState} from "react"
 
@@ -17,27 +18,57 @@ const covers = [ProductOne, ProductTwo, ProductThree, ProductFour]
 const thumbnails = [ProductOneThumbnail, ProductTwoThumbnail, ProductThreeThumbnail, ProductFourThumbnail]
 
 export function ProductDisplay() {
+
     const [coverIndex, setCoverIndex] = useState(0)
+    const [coverPage, setCoverPage] = useState(false)
+
+    function DisplaySection() {
+        return (
+            <>
+            <div className="row">
+            <DisplayButton source={covers[coverIndex]} description="product cover"
+            onClick={() => setCoverPage(!coverPage)}/>
+            <DisplayButton source={IconNext} description="next"
+            onClick={() => setCoverIndex(prev => Math.min(prev + 1, covers.length-1))}/>
+            <DisplayButton source={IconPrevious} description="previous"
+            onClick={() => setCoverIndex(prev => Math.max(prev - 1, 0))}/>
+        </div>
+        <div className="row image-section">
+                {thumbnails.map((image, index) => (
+                    <DisplayButton className={coverIndex === index ? "is-active" : ""}
+                    name={`button_${index}`}
+                    key={index}
+                    source={image}
+                    description="product thumbnail"
+                    onClick={() => setCoverIndex(index)} />
+                ))}
+        </div>
+        </>
+        )
+    }
+
+    function CoverPage() {
+        return (
+            <>
+                <Overlay bgColor="rgb(0 0 0 / 50%)"/>
+                <div
+                className="cover-page">
+                    <DisplayButton
+                    source={IconClose}
+                    description="close overlay"
+                    onClick={() => setCoverPage(!coverPage)} />
+                    <DisplaySection />
+                </div>
+            </>
+            
+        )
+    }
+
     return (
         <section>
+            {coverPage && <CoverPage />}
             <Container>
-                <div className="row">
-                    <DisplayButton source={covers[coverIndex]} description="product cover" />
-                    <DisplayButton source={IconNext} description="next"
-                    onClick={() => setCoverIndex(prev => Math.min(prev + 1, covers.length-1))}/>
-                    <DisplayButton source={IconPrevious} description="previous"
-                    onClick={() => setCoverIndex(prev => Math.max(prev - 1, 0))}/>
-                </div>
-                <div className="row image-section">
-                        {thumbnails.map((image, index) => (
-                            <DisplayButton className={coverIndex === index ? "is-active" : ""}
-                            name={`button_${index}`}
-                            key={index}
-                            source={image}
-                            description="product thumbnail"
-                            onClick={() => setCoverIndex(index)} />
-                        ))}
-                </div>
+                <DisplaySection />
             </Container>
         </section>
     )
